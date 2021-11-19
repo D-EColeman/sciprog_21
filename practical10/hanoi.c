0defcba96a28278c979b33198ac48d74a4cf1ef6
Diarmuid Coleman <117325526@umail.ucc.ie>
2021-11-19 19:27:27 +0000

    Added updated files relating to practical 10

--- practical10/hanoi.c
+++ practical10/hanoi.c
@@ -5,16 +5,20 @@ void hanoi(int n, int start, int mid, int end);
 
 int main(){
 
+	//user defines the number of disks for the problem
 	int h;
 	printf("Enter the number of disks: ");
 	scanf("%d", &h);
 
+	//starts the solving problem
+	//the pegs are denoted 1, 2, 3
 	printf("\n\nThe Solution to the Tower of Hanoi problem with %d disks:\n", h);
 	hanoi(h, 1, 2, 3);
 
 	return 0;
 }
 
+//a recursive function to solve the ToH problem
 void hanoi(int n, int start, int mid, int end){
 	if(n == 1){
 		printf("Moved disk %d from %d to %d\n", n, start, end);
